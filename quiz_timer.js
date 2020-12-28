7512de18bb56e2c5ad0a8a4944107ab2f383d8ec
SHIVA SANKARI C <shivasankari3101@gmail.com>
2020-12-28 17:26:19 +0530

    Color changes

--- quiz_timer.js
+++ quiz_timer.js
@@ -1,11 +1,10 @@
 var countdownNumberEl = document.getElementById('countdown-number');
-var countdown =30;
+var countdown =3;
 
 countdownNumberEl.textContent = countdown;
 
 setInterval(function() {
   countdown--;
-  countdown=String(countdown).padStart("2","0");
   if(countdown>=0){
     countdownNumberEl.textContent = countdown;
   }
